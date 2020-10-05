689123a94a78f3f37650bcbaaeded56c117c234b
ishwariv <70537425+ishwariv@users.noreply.github.com>
2020-10-05 15:12:47 -0400

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,13 @@
+class Bird extends Base{
+    constructor(x,y) {
+      //keyword to inherit the contructor properties of base class
+      super(x,y,50,50);
+
+      this.image = loadImage("sprites/bird.png");
+    }
+    display(){
+      this.body.position.x=mouseX;
+      this.body.position.y=mouseY;
+      super.display();
+    }
+  }
